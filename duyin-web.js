48d8f91c5b8b01add1c46d64317b251870af14f2
chimenchen <chimenchen@gmail.com>
2026-07-21 19:32:05 +0800

    口口聲聲：轉泰語等，修正部分問題，測試上古NOCM擬音轉換。

--- duyin-web.js
+++ duyin-web.js
@@ -872,17 +872,17 @@ function __wbg_get_imports() {
             console.warn(arg0, arg1, arg2, arg3);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref, Externref], shim_idx: 159, ret: Result(Externref), inner_ret: Some(Result(Externref)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref, Externref], shim_idx: 155, ret: Result(Externref), inner_ret: Some(Result(Externref)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen_d1e34223b88a25b8___convert__closures_____invoke___wasm_bindgen_d1e34223b88a25b8___JsValue__wasm_bindgen_d1e34223b88a25b8___JsValue__core_7d5f0a2ba6a62c33___result__Result_wasm_bindgen_d1e34223b88a25b8___JsValue__wasm_bindgen_d1e34223b88a25b8___JsValue___true_);
             return ret;
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 161, ret: Externref, inner_ret: Some(Externref) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 153, ret: Externref, inner_ret: Some(Externref) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen_d1e34223b88a25b8___convert__closures_____invoke___wasm_bindgen_d1e34223b88a25b8___JsValue__wasm_bindgen_d1e34223b88a25b8___JsValue__true_);
             return ret;
         },
         __wbindgen_cast_0000000000000003: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 164, ret: Boolean, inner_ret: Some(Boolean) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 157, ret: Boolean, inner_ret: Some(Boolean) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen_d1e34223b88a25b8___convert__closures_____invoke___wasm_bindgen_d1e34223b88a25b8___JsValue__bool__true_);
             return ret;
         },
